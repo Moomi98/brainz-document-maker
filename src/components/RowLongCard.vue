d06a3068bf62a0f1359b845be957a85c05689d96
Moomi98 <kymkjh2002@gmail.com>
2023-07-17 21:05:14 +0900

    home에 card 적용

---
+++ src/components/RowLongCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card class="mx-auto w-50 h-100">
+    <v-container class="h-100">
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            v-for="item in props.items"
+            :key="item.title"
+            color="white"
+            theme="dark"
+            class="d-flex card"
+          >
+            <div>
+              <v-avatar class="ma-3" size="125" rounded="0">
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+                ></v-img>
+              </v-avatar>
+            </div>
+            <div>
+              <v-card-title class="text-h5"> {{ item.title }} </v-card-title>
+
+              <v-card-subtitle>{{ item.subtitle }}</v-card-subtitle>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+interface itemProps {
+  title: string;
+  subtitle: string;
+}
+const props = defineProps({
+  items: {
+    type: Array<itemProps>,
+    required: true,
+  },
+});
+</script>
+<style lang="scss" scoped>
+.card {
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <RowLongCard :items="items" />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import RowLongCard from "@/components/RowLongCard.vue";
+
+const items = [
+  {
+    title: "주간 업무 보고서",
+    subtitle: "매주 작성하는 주간 업무 보고서를 더욱 편하게 작성해보세요.",
+  },
+];
+</script>
